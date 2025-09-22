395705b3884453caf3032f4379a82f5da5357d46
LauraBLei <laura.blei@outlook.com>
2025-09-22 11:35:32 +0200

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { HeaderComponent } from "@/components/header";
+import { HeroSection } from "@/components/sections/heroSection";
 
 export default function Home() {
   return (
     <div>
       <HeaderComponent />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/sections/heroSection.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export const HeroSection = () => {
+  return (
+    <div className="w-full relative h-auto shadow-lg">
+      <div className="relative w-full h-[370px] overflow-hidden flex items-center">
+        <Image
+          src={"/hero.png"}
+          alt="Nails"
+          fill
+          className="object-cover"
+          quality={100}
+          priority
+          unoptimized
+        />
+        <div className="absolute z-20 text-white text-2xl flex flex-col gap-2 ml-30">
+          <p>
+            Nails <span className="text-base">By</span> Kate
+          </p>
+          <p>Beautiful Nails, Every Time</p>
+          <a href="#booking" className="button mt-2">
+            Book Your Appointment Today
+          </a>
+        </div>
+        <div className="absolute inset-0 bg-black/30"></div>
+      </div>
+    </div>
+  );
+};
