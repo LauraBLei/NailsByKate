3c27fbe13c5c2a9691753fa0e5c02a0f1055b027
LauraBLei <laura.blei@outlook.com>
2025-09-24 13:27:08 +0200

    om meg responsiveness

--- src/components/sections/om.tsx
+++ src/components/sections/om.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export const OmMegSection = () => {
   return (
-    <section id="ommeg" className="bg-brand-SecondPink w-full p-10 flex">
+    <section
+      id="ommeg"
+      className="bg-brand-SecondPink w-full p-10 flex flex-col md:flex-row"
+    >
       <div className="flex-1 flex flex-col gap-10">
         <h2 className="text-2xl">OM MEG</h2>
         <div className="flex flex-col gap-4">
@@ -25,7 +28,7 @@ export const OmMegSection = () => {
         <p></p>
       </div>
       <div className="flex-1">
-        <div className="relative w-100 h-100 mx-auto ">
+        <div className="relative w-70 h-70 md:w-100 md:h-100 mx-auto ">
           <Image
             src="/profile.jpg"
             alt="Image of Katerina"
